98a8b4e366aaac7466b3fb669bcaf651710f24f1
landtlord <andy.landtsheer@pandora.be>
2020-10-20 20:26:29 +0200

    Initial commit

---
+++ diagnose/src/main/java/be/hogent/diagnose/DiagnoseApplication.java
@@ -0,0 +1,13 @@
+package be.hogent.diagnose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnoseApplication.class, args);
+	}
+
+}
---
+++ diagnose/src/test/java/be/hogent/diagnose/DiagnoseApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hogent.diagnose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
